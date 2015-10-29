4b83f15aaf72781c9270f2abab66bc3c8ecfc204
mariog86 <mariog86@gmail.com>
2015-10-29 10:11:36 +0100

    Added failing test.

--- FractionLibrary/FractionTest.cs
+++ FractionLibrary/FractionTest.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace FractionLibrary
 {
-    public class Class1
+    [TestFixture]
+    public class FractionTest
     {
+        [Test]
+        public void FirstTest()
+        {
+        }
     }
 }
